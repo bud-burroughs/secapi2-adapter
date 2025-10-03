600597ca847edb3a9fa4a7f704dfdaaf5363e944
bud-burroughs <50154439+bud-burroughs@users.noreply.github.com>
2025-10-03 10:29:59 -0400

    More delete fixes

--- src/sec_adapter_bundle.c
+++ src/sec_adapter_bundle.c
@@ -123,7 +123,7 @@ static Sec_Result Sec_StoreBundleData(Sec_ProcessorHandle* processorHandle, SEC_
             return SEC_RESULT_FAILURE;
         }
 
-        SecBundle_Delete(processorHandle, object_id);
+        //SecBundle_Delete(processorHandle, object_id);
 
         char file_name_bundle[SEC_MAX_FILE_PATH_LEN];
         char file_name_verification[SEC_MAX_FILE_PATH_LEN];
